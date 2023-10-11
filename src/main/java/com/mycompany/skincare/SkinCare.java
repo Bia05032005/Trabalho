c66859db6d5fd928d2b4549aaf9ec3379cddffb6
BEATRIZ SOUZA GOMES <bgomes@labcetec.net>
2023-10-11 11:32:20 -0300

    first commit

---
+++ src/main/java/com/mycompany/skincare/SkinCare.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.skincare;
+
+/**
+ *
+ * @author bgomes
+ */
+public class SkinCare {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
